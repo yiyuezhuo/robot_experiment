58cd6ce2751860523396180b021a31cce2fcb345
yiyuezhuo <616271835@163.com>
2019-06-07 20:27:09 +0800

    add package related content

--- robot2.py
+++ robot2.py
@@ -4,38 +4,43 @@ import numpy as np
 #print('loading....')
 
 def propeller(x,y,z,rot):
-    bpy.ops.mesh.primitive_cylinder_add(radius=0.02, depth=0.1, location = (x,y,z,0.05))
+    bpy.ops.mesh.primitive_cylinder_add(radius=0.02, depth=0.1, location = (x,y,z+0.05))
     
-    bpy.ops.mesh.primitive_cube_add(radius=0.15, location = (x,y,z,0.1-0.005))
+    bpy.ops.mesh.primitive_cube_add(radius=0.15, location = (x,y,z+0.1-0.005))
     bpy.context.object.scale[1] = 0.1333
     bpy.context.object.scale[2] = 0.0033
     
     bpy.context.object.rotation_euler[2]=rot
 
-def arm(rot):
-    bpy.ops.mesh.primitive_cube_add(radius=0.494,location=(0.5,0.5,-0.0250))
+def arm(x,y , rot):
+    bpy.ops.mesh.primitive_cube_add(radius=0.494,location=(x, y, -0.0250))
     bpy.context.object.scale[1] = 0.1417/2
     bpy.context.object.scale[2] = 0.1012/2
     
     bpy.context.object.rotation_euler[2]=rot
 
-def shell():
+def shell(x=0.5,y=0.5):
     print('start wtf')
-    bpy.ops.mesh.primitive_cylinder_add(radius=0.14, depth=0.2, location = (0.5,0.5,0.0))
+    bpy.ops.mesh.primitive_cylinder_add(radius=0.14, depth=0.2, location = (x, y, 0.0))
     
-    bpy.ops.mesh.primitive_cylinder_add(radius=0.07, depth=0.2, location = (0.15,0.85,0.0))
-    bpy.ops.mesh.primitive_cylinder_add(radius=0.07, depth=0.2, location = (0.85,0.85,0.0))
-    bpy.ops.mesh.primitive_cylinder_add(radius=0.07, depth=0.2, location = (0.85,0.15,0.0))
-    bpy.ops.mesh.primitive_cylinder_add(radius=0.07, depth=0.2, location = (0.15,0.15,0.0))
+    bpy.ops.mesh.primitive_cylinder_add(radius=0.07, depth=0.2, location = (x-0.35, y+0.35, 0.0))
+    bpy.ops.mesh.primitive_cylinder_add(radius=0.07, depth=0.2, location = (x+0.35, y+0.35, 0.0))
+    bpy.ops.mesh.primitive_cylinder_add(radius=0.07, depth=0.2, location = (x+0.35, y-0.35, 0.0))
+    bpy.ops.mesh.primitive_cylinder_add(radius=0.07, depth=0.2, location = (x-0.35, y-0.35, 0.0))
     
-    arm(np.pi/4)
-    arm(-np.pi/4)
+    arm(x, y, np.pi/4)
+    arm(x, y, -np.pi/4)
     
-def nice_boat():
-    shell()
-    propeller(0.15,0.85,0.1,-np.pi/4)
-    propeller(0.85,0.85,0.1,np.pi/4)
-    propeller(0.85,0.15,0.1,-np.pi/4)
-    propeller(0.15,0.15,0.1,np.pi/4)
-    
-nice_boat()
+def nice_boat(x=0.5,y=0.5):
+    shell(x=x,y=y)
+    propeller(x-0.35, y+0.35, 0.1, -np.pi/4)
+    propeller(x+0.35, y+0.35, 0.1,  np.pi/4)
+    propeller(x+0.35, y-0.35, 0.1, -np.pi/4)
+    propeller(x-0.35, y-0.35, 0.1,  np.pi/4)
+
+# show example part    
+#nice_boat(x=0,y=0)
+# show shell
+#shell(x=0,y=0)
+# show propeller
+#propeller(0,0,0,0)
---
+++ scripts/state_publisher.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from sensor_msgs.msg import JointState
+import rospy
+import numpy as np
+from geometry_msgs.msg import TransformStamped
+import tf
+
+print 'start python moving script'
+
+if __name__ == '__main__':
+    rospy.init_node('state_publisher')
+
+    joint_pub = rospy.Publisher('joint_states', JointState, queue_size=1)
+
+    broadcaster = tf.TransformBroadcaster()
+    loop_rate = rospy.Rate(30)
+
+    degree = np.pi / 180
+
+    # robot state
+    tilt = 0.0
+    tinc = degree
+    swivel = 0.0
+    angle = 0.0
+    height = 0.0
+    hinc = 0.005
+
+    # message declarations
+
+    joint_state = JointState()
+
+    while not rospy.is_shutdown():
+        # update joint_state
+        joint_state.header.stamp = rospy.Time.now()
+        
+        joint_state.name = ['p1', 'p2', 'p3', 'p4']
+        joint_state.position = [swivel, swivel, swivel, swivel]
+
+        # update transform
+        # (moving in a circle with radius=2)
+        # Those logic is transfered into odom_trans.sendTrasform
+
+        # send the joint state and transform
+        joint_pub.publish(joint_state)
+        broadcaster.sendTransform(
+                (np.cos(angle)*2, np.sin(angle)*2, 0.7),
+                tf.transformations.quaternion_from_euler(0, 0, angle+np.pi/2),
+                rospy.Time.now(),
+                'drone_body',
+                'odom'
+                )
+
+        # Create new robot state
+        tilt += tinc
+        if tilt < -0.5 or tilt > 0:
+            tinc *= -1
+        height += hinc
+        if height > 0.2 or height < 0:
+            hinc *= -1
+        swivel += degree
